9e3a84862e62f431b293d208892e7629de05d5ef
rafiftrlh <rafifitro89@gmail.com>
2024-02-28 21:07:18 +0700

    feat: register and push user data to local storage

--- js/User.js
+++ js/User.js
@@ -1,11 +1,23 @@
 // Untuk mengurus business logic
 
 class User {
+  constructor() {
+    this.users = this.getUsers() || [];
+  }
+
   saveUser(userData) {
     const newUser = {
       id: Date.now(),
       ...userData,
     };
-    console.log(userData);
+
+    this.users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(this.users));
+
+    return (window.location.href = "/signin.html");
+  }
+
+  getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
   }
 }
--- js/addUser.js
+++ js/addUser.js
@@ -1,1 +1,16 @@
 // Penghubung ui dan model
+
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.getElementById("userForm");
+  const userManager = new User();
+
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userData = {
+      username: document.getElementById("username").value,
+    };
+
+    userManager.saveUser(userData);
+  });
+});
